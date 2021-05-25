3e5c7d56b137f1c18cf0269f8e97bc8fc8fe4791
danilgansky <danylo.hanskyi@computools.com>
2021-05-25 15:16:05 +0300

    Init commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module k8s-test
+
+go 1.13
+
+require (
+	github.com/thedevsaddam/renderer v1.2.0
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/thedevsaddam/renderer"
+)
+
+var rnd *renderer.Render
+
+func init() {
+	opts := renderer.Options{
+		ParseGlobPattern: "./public/*.html",
+	}
+
+	rnd = renderer.New(opts)
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("request from %s at %s", r.RemoteAddr, time.Now().Format("Mon Jan 2 15:04:05 2006"))
+
+	err := rnd.Template(w, http.StatusOK, []string{"./public/index.tpls"}, map[string]interface{}{"ip": r.RemoteAddr})
+	if err != nil {
+		log.Fatalf("error in %s: %s", r.RemoteAddr, err.Error())
+	}
+}
+
+func main() {
+	const port = ":8000"
+
+	http.HandleFunc("/", homeHandler)
+	log.Printf("Server started at %s...", port)
+
+	if err := http.ListenAndServe(port, nil); err != nil {
+		log.Fatal(err)
+	}
+}
