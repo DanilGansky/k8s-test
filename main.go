cca5f30d8780864fb34983ecb285d4b722c27362
danilgansky <danylo.hanskyi@computools.com>
2021-05-26 15:19:46 +0300

    Fixed showing host ip

--- main.go
+++ main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"log"
+	"net"
 	"net/http"
 	"time"
 
 	"github.com/thedevsaddam/renderer"
 )
 
-var rnd *renderer.Render
+var (
+	rnd     *renderer.Render
+	localIP string
+)
 
 func init() {
 	opts := renderer.Options{
@@ -16,14 +20,26 @@ func init() {
 	}
 
 	rnd = renderer.New(opts)
+	localIP = getOutboundIP().String()
+}
+
+func getOutboundIP() net.IP {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+	return localAddr.IP
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	log.Printf("request from %s at %s", r.Host, time.Now().Format("Mon Jan 2 15:04:05 2006"))
+	log.Printf("request from %s at %s", localIP, time.Now().Format("Mon Jan 2 15:04:05 2006"))
 
-	err := rnd.Template(w, http.StatusOK, []string{"./public/index.tpls"}, map[string]interface{}{"ip": r.Host})
+	err := rnd.Template(w, http.StatusOK, []string{"./public/index.tpls"}, map[string]interface{}{"ip": localIP})
 	if err != nil {
-		log.Fatalf("error in %s: %s", r.RemoteAddr, err.Error())
+		log.Fatalf("error in %s: %s", localIP, err.Error())
 	}
 }
 
